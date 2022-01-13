cd9ef7d5ea1837d1704ef3bcf7c9bc54383085c7
Konrad Scherb <konrad.scherb@gmx.de>
2022-01-13 10:36:40 +0100

    feat: add form element

--- src/components/AddTodo/index.jsx
+++ src/components/AddTodo/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
+import { v4 as uuid } from "uuid";
 
 const StyledTextField = styled(TextField)`
 	width: 100%;
@@ -10,21 +11,12 @@ const StyledTextField = styled(TextField)`
 const AddTodo = ({ text, setText, setTodos, todos }) => {
 	return (
 		<>
-			<StyledTextField
-				size="small"
-				type="text"
-				value={text}
-				onChange={event_ => {
-					setText(event_.target.value);
-				}}
-				label="Add Todo"
-			/>
-			<Button
-				variant="contained"
-				disabled={!text}
-				onClick={() => {
+			<form
+				onSubmit={event_ => {
+					event_.preventDefault();
 					const newArray = [...todos];
 					newArray.push({
+						id: uuid(),
 						name: text,
 						checked: false,
 						isEditing: false,
@@ -34,8 +26,19 @@ const AddTodo = ({ text, setText, setTodos, todos }) => {
 					setText("");
 				}}
 			>
-				Add
-			</Button>
+				<StyledTextField
+					size="small"
+					type="text"
+					value={text}
+					onChange={event_ => {
+						setText(event_.target.value);
+					}}
+					label="Add Todo"
+				/>
+				<Button type="submit" variant="contained" disabled={!text}>
+					Add
+				</Button>
+			</form>
 		</>
 	);
 };
